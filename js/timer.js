dd979e2aad2dc32291990ca51a44449d5905e856
Mario Rangga Baihaqi <62511453+komarr007@users.noreply.github.com>
2022-10-27 19:27:07 +0700

    Add files via upload

---
+++ js/timer.js
@@ -0,0 +1,23 @@
+function updateTimer() {
+    future  = Date.parse("November 12, 2022 12:00:00");
+    now     = new Date();
+    diff    = future - now;
+  
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+  
+    d = days;
+    h = hours - days  * 24;
+    m = mins  - hours * 60;
+    s = secs  - mins  * 60;
+  
+    document.getElementById("timer")
+      .innerHTML =
+         d + '<span> days</span> ' +
+         h + '<span> hours</span> ' +
+         m + '<span> minutes</span> ' +
+         s + '<span> seconds</span> ' ;
+  }
+  setInterval('updateTimer()', 1000 );
